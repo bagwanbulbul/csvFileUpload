387f09a1fd52eb6e6e001d81d72fe55afbb512d7
bagwanbulbul <bulbulbagwan918@gmail.com>
2020-11-05 15:38:38 +0530

    get user Details

--- src/routes/csv.routes.js
+++ src/routes/csv.routes.js
@@ -10,4 +10,34 @@ let routes = (app) => {
   app.use("/api/csv", router);
 };
 
+router.get("/get_user_Details",(req,res)=>{
+  var user_details = Select("first_name","last_name","job_title","email","age").from("information")
+  user_details.then((data)=>{
+    res.send(data)
+    console.log(data)
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+
+})
+
+router.get("/count_job_title",(req,res)=>{
+  var job_title = select("job_title").from("information")
+  job_title.then((data)=>{
+    var count = 0;
+    for(var i of data){
+      if(data[i]["job_tittle"] == data[i+1]["job_title"]){
+        conunt = count+1
+      }
+    }
+    res.send(count)
+    console.log(count)
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+
+})
+
 module.exports = routes;
